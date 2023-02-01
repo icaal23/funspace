b5a23df59e42063b317443464045d37d6501ea19
icaal23 <123912919+icaal23@users.noreply.github.com>
2023-02-01 14:00:44 +0800

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,8 @@
+#Hitung Luas Persegi Panjang
+    def luas_persegi_panjang():
+        p = int (input("Masukkan Panjang Persegi"))
+        l = int (input("Masukkan Lebar Persegi"))
+
+        luas = p * l
+        print("Luas Persegi adalah: ", luas)
+    luas_persegi_panjang()
